660a0bdba8c49f766557aeadd9dc811d8802fb2d
David Ko <dko@suse.com>
2019-12-24 01:58:33 +0800

    Update kind scripts

---
+++ k8s/kind/destroy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+DIR=$(dirname "$(realpath "$0")")
+# shellcheck disable=SC2164
+cd "$DIR"
+
+kind delete cluster --name mc
--- k8s/kind/setup.sh
+++ k8s/kind/setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # https://rook.github.io/docs/rook/master/ceph-quickstart.html#deploy-the-rook-operator
 
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
 DIR=$(dirname "$(realpath "$0")")
 # shellcheck disable=SC2164
 cd "$DIR"
 
-kubectl create -f mutliple_nodes_cluster.yaml
+kind create cluster --name mc --config="$(pwd)/mutliple_nodes_cluster.yaml"
