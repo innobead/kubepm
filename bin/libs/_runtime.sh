18f77f9280c1f5e33a2b00fb98703a3bf58db8c3
David Ko <dko@suse.com>
2020-02-21 00:37:44 +0800

    Update LXC installation

--- bin/libs/_runtime.sh
+++ bin/libs/_runtime.sh
@@ -54,20 +54,65 @@ function install_vagrant() {
 }
 
 function install_lxc() {
-    pkgs=(
-      lxc
-      # lxd
-    )
-    zypper_pkg_install "${pkgs[@]}"
+  cmd=install
+  if check_cmd lxd; then
+    cmd=refresh
+  fi
+  sudo snap "$cmd" --classic lxd
 
-    cmd=install
-    if check_cmd lxd; then
-      cmd=refresh
-    fi
-    sudo snap "$cmd" --classic lxd
+  sudo usermod -a -G lxd "${KU_USER}"
+  sudo systemctl start snap.lxd.daemon.service
+  sudo systemctl enable snap.lxd.daemon.service
 
-    sudo usermod -a -G lxd "${KU_USER}"
-    # newgrp lxd
-    sudo systemctl enable snap.lxd.daemon.service
-    sudo systemctl start snap.lxd.daemon.service
+  # when running `lxc list image images:` and encoutering a url unresolved issue, please reload snap.lxd.deamon. It's because network ready after snap for some reason.
+  # sudo systemctl reload snap.lxd.daemon
+
+  cat<<EOF
+Use `lxc help` to learn how to manage container and images.
+
+lxc init: Please relogin to take effect all installation, then execute below commands.
+
+  cat >lxd.yaml <<\EOF
+config: {}
+networks:
+- config:
+    ipv4.address: 172.22.0.1/16
+    ipv4.nat: "true"
+    ipv6.address: auto
+  description: ""
+  managed: false
+  name: lxdbr0
+  type: ""
+storage_pools:
+- config: {}
+  description: ""
+  name: default
+  driver: dir
+profiles:
+- config: {}
+  description: ""
+  devices:
+    eth0:
+      name: eth0
+      nictype: bridged
+      parent: lxdbr0
+      type: nic
+    root:
+      path: /
+      pool: default
+      type: disk
+  name: default
+cluster: null
+\EOF
+
+  cat lxd.yaml | lxd init --preseed - && rm lxd.yaml
+
+Commands:
+  lxc list image images:
+  lxc launch images:opensuse/tumbleweed/amd64 opensuse
+  lxc exec opensuse -- passwd root
+  lxc config set opensuse limits.memory 1GB
+  lxc exec opensuse -- free -h
+  lxc delete opensuse --force
+EOF
 }
